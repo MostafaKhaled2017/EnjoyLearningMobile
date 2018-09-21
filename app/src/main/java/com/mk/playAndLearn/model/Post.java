33bfa83f97b4f8545ae93e52821267e7167f9015
MostafaKhaled2017 <mostafakhaled835@outlook.com>
2018-09-21 22:28:46 +0200

    initial commit

---
+++ app/src/main/java/com/mk/playAndLearn/model/Post.java
@@ -0,0 +1,37 @@
+package com.mk.playAndLearn.model;
+
+public class Post {
+    private String userName, message, date, imageUrl;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
