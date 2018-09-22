2e06ee2ae05135ff8d8b73e5f3ce25d7094d0adb
MostafaKhaled2017 <mostafakhaled835@outlook.com>
2018-09-22 02:05:15 +0200

    initial commit

---
+++ app/src/main/java/com/mk/playAndLearn/model/Lesson.java
@@ -0,0 +1,47 @@
+package com.mk.playAndLearn.model;
+
+public class Lesson {
+    String content, title, position, arabicPosition, subject;
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getArabicPosition() {
+        return arabicPosition;
+    }
+
+    public void setArabicPosition(String arabicPosition) {
+        this.arabicPosition = arabicPosition;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getTitle() {
+
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
