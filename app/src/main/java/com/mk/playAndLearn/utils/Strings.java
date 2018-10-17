a4a19e17b90efca05224d51e788f1bda4cb7e886
MostafaKhaled2017 <mostafakhaled835@outlook.com>
2018-10-17 16:53:52 +0200

    initial commit

--- app/src/main/java/com/mk/playAndLearn/utils/Strings.java
+++ app/src/main/java/com/mk/playAndLearn/utils/Strings.java
@@ -19,6 +19,9 @@ public class Strings {
     public static String wonChallengeText = "ربحت";
     public static String loseChallengeText = "خسرت";
     public static String drawChallengeText = "تعادل";
+    public static String[] subjectsArray = {"اختر المادة", "لغة عربية", "لغة انجليزية"
+            , "جغرافيا", "تاريخ", "Physics", "Chemistry"
+            , "Biology", "Algebra", "Geometry", "Trigonometry"};
 
 
 }
